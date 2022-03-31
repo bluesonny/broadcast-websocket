0758a9073672ec894b180acbbd4ad5fb34127961
bluesonny <bluesonny2009@gmail.com>
2022-03-31 16:49:13 +0800

    fix

--- handler/client.go
+++ handler/client.go
@@ -2,9 +2,11 @@ package handler
 
 import (
 	"context"
+	"errors"
 	"log"
 	"nhooyr.io/websocket"
 	"nhooyr.io/websocket/wsjson"
+	"time"
 )
 
 type Client struct {
@@ -19,18 +21,30 @@ func (c *Client) Read(ctx context.Context, manager clientManager) {
 	defer func() {
 		manager.unregister <- c
 		//c.socket.Close()
-		c.socket.Close(websocket.StatusInternalError, "读关闭")
+		c.socket.Close(websocket.StatusNormalClosure, "正常读关闭")
 		log.Printf("defer 读关闭 %v\n", c)
 	}()
+	var (
+		receiveMsg map[string]string
+		err        error
+	)
 
 	for {
-		//_, _, err := c.socket.ReadMessage()
-		err := wsjson.Read(ctx, c.socket, "")
-		log.Printf("是在不停的读吗？ %v\n", c)
+		err = wsjson.Read(ctx, c.socket, &receiveMsg)
+		//log.Printf("是在不停的读吗，连接对象？ %v\n", c)
+		log.Printf("是在不停的读吗，收到客户端消息？ %v\n", receiveMsg)
 		if err != nil {
-			log.Println("连接断开了...")
+			var closeErr websocket.CloseError
+			if errors.As(err, &closeErr) {
+				log.Printf("是在不停的读吗，正常关闭")
+				break
+			}
+
+			log.Printf("是在不停的读吗，读取出错关闭？ %v\n", err)
 			break
 		}
+		sendMsg := &Message{Status: "ok", Content: receiveMsg["content"], ClientSendTime: time.Now()}
+		wsjson.Write(ctx, c.socket, sendMsg)
 
 	}
 }
@@ -62,7 +76,7 @@ func (c *Client) Write(ctx context.Context, manager clientManager) {
 			if err != nil {
 				manager.unregister <- c
 				//c.socket.Close()
-				c.socket.Close(websocket.StatusInternalError, "写关闭")
+				c.socket.Close(websocket.StatusNormalClosure, "写关闭")
 				log.Println("写不成功数据就关闭了")
 				break
 			}
